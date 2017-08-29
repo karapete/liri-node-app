e834783802cf79335a99e773a920576d2237943c
Kara Peterson <karapeterson@karas-macbook.uconnect.utah.edu>
2017-08-28 21:25:36 -0600

    maybe it will work

--- keys.js
+++ keys.js
@@ -1,9 +1,14 @@
-exports.twitterKeys = {
-  consumer_key: 'LWO2ARmURQSq8OjpUNtEfb6M7',
-  consumer_secret: '2djGLfWhCYT9lp39aBpjDMVJ59H7vHpNdWJmFJZW9kS4jWcGfi',
-  access_token_key: '900113249391091716-zHkyXfLDhX8seLAXoGq0u86Fmdh8plC',
-  access_token_secret: 'CzggvWwyeSti9MorHNdpkoeU3g9Y0LTyM6K7iwHZGFXjA',
+
+
+
+
+
+module.exports = {
+  consumer_key:         'LWO2ARmURQSq8OjpUNtEfb6M7',
+  consumer_secret:      '2djGLfWhCYT9lp39aBpjDMVJ59H7vHpNdWJmFJZW9kS4jWcGfi',
+  access_token:         '900113249391091716-zHkyXfLDhX8seLAXoGq0u86Fmdh8plC',
+  access_token_secret:  'CzggvWwyeSti9MorHNdpkoeU3g9Y0LTyM6K7iwHZGFXjA',
+
+
+
 }
-
-
-
--- liri.js
+++ liri.js
@@ -1,46 +1,53 @@
 // array of commands
-
-var commandArray = ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"]
-
+var commandArray = ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"];
 
 
 
-//twitter
-var Twitter = require('twitter');
-var inquirer =require("inquirer");
+//twitter keys, path to keys.js file
+var Twit = require('twit');
+var keys = require('./keys');
+var T = new Twit(keys);
 
-var tweets = function() {}
+//need to pull tweets from my twitter
+//add prompts to ask quetions
 
-var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
-});
- 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
+var inquirer = require("inquirer");
+
 inquirer.prompt([
 
   {
-  	type: "checkbox",
-    name: "carryingWhat",
-    message: "What would you like to do?",
-    choices: ["my-tweets", "spotify-this-song", "movie-this", "do"]
-  },
-
-
+    type: "list",
+    name: "whatCommand",
+    message: "What would you like to do??",
+    choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"]
+  }
 
 ]).then(function(user) {
 
+  //if the user choose's tweets
+  if (user.whatCommand === "my-tweets") {
+  		console.log("You can see my tweets!")
+  		var searchTwitter = {
+				q: 'beverly_tart',
+				count:20
+}
 
+	T.get('search/tweets', searchTwitter, gotData)
 
-
-
+	function gotData(err, data, response) {
+			var tweets = data.statuses;
+			for (var i = 0; i < tweets.length; i++) {
+			console.log(tweets[i].text);
+	   } 
+	 }
+}
+  else{
+  	console.log("Fine don't look at my tweets!")
+  }
+  if (user.whatCommand === "spotify-this-song") {
+  	console.log("Play that funky music white boy")
+  }
+});
 
 
 
@@ -49,8 +56,8 @@ inquirer.prompt([
 // var Spotify = require('node-spotify-api');
  
 // var spotify = new Spotify({
-//   id: <your spotify client id>,
-//   secret: <your spotify client secret>
+//   id: <d2836498c70d41abb5acdb056492b2c7>,
+//   secret: <1592c8f35dbb43caa04068c25e6feb8a>
 // });
  
 // spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
@@ -60,3 +67,21 @@ inquirer.prompt([
  
 // console.log(data); 
 // });
+
+// search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
+
+
+
+// code to post tweets from node.js
+// var tweet = {
+// 	status: 'a tweet from nodejs!'
+// }
+
+// T.post('statuses/update', tweet, tweeting);
+
+//  function tweeting(err, data, response) {
+//   console.log(data)
+// }
+
+
+
